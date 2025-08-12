650e16743827097338c7c8e118141fb6e01d9109
farazsheraz <farazahmed9094@gmail.com>
2025-08-12 05:21:28 +0500

    koi responsive

--- components/IndustriesWeServe.js
+++ components/IndustriesWeServe.js
@@ -52,10 +52,10 @@ const industries = [
 ];
 export default function IndustriesWeServe({ }) {
     return (
-        <ul className='IndustriesWeServe grid grid-cols-3 sm:grid-cols-3 md:grid-cols-6 gap-4'>
+        <ul className='IndustriesWeServe grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-4'>
             {industries.map((item, index) => (
                 <li key={index} style={{ flex: '0 0 15%' }}
-                    className="bg-[#4966552e] border-[#dbe7d9] flex flex-col items-center justify-between gap-2 min-w-0 transition-all duration-500 ease-in-out border rounded-[19px] p-2.5 font-medium text-lg hover:scale-105 cursor-pointer">
+                    className="bg-[#4966552e] border-[#dbe7d9] flex flex-col items-center justify-between gap-2 min-w-0 transition-all duration-500 ease-in-out border rounded-[19px] p-4 md:p-2.5 font-medium text-lg hover:scale-105 cursor-pointer">
                     <div>
                         <Image
                             src={item.img}
