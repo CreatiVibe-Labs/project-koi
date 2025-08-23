11ceb634e46a5caea09280f5f6873a0f4a6c5691
farazsheraz <farazahmed9094@gmail.com>
2025-08-23 15:31:43 +0500

    koi responsive

--- app/contact/page.js
+++ app/contact/page.js
@@ -17,22 +17,22 @@ export default function ContactPage({ imageUrl, title, text }) {
                 <div className="grid md:grid-cols-2 grid-cols-1 w-full gap-5">
                     <div className="contactBg backdrop-blur-lg border-0 border-amber-50 rounded-2xl md:p-10 p-2.5 flex items-end">
                         <ul>
-                            <li className="flex items-center gap-5">
+                            <li className="flex items-center gap-2.5 md:gap-5 ">
                                 <div className="bg-white rounded-full p-2"><Image alt="icon" src="/icons/phone.png" width={30} height={30}></Image></div>
                                 <Link href="tel:+81788552760" className="text-white custom-shaodw" >
                                     +81 78 855 2760</Link>
                             </li>
-                            <li className="flex items-center gap-5">
-                                <div className="bg-white rounded-full px-2 py-2.5"><Image alt="icon" src="/icons/mail.png" width={30} height={15}></Image></div>
+                            <li className="flex items-center gap-2.5 md:gap-5">
+                                <div className="bg-white rounded-full md:px-2 md:py-2.5 p-2"><Image alt="icon" src="/icons/mail.png" width={30} height={15}></Image></div>
                                 <Link href="mailto:inquiry@aerialink.jp" className="flex items-center gap-5 text-white custom-shaodw">inquiry@aerialink.jp</Link>
                             </li>
-                            <li className="flex items-center gap-5">
+                            <li className="flex items-center gap-2.5 md:gap-5">
                                 <div className="bg-white rounded-full p-2"><Image alt="icon" src="/icons/web.png" width={30} height={30}></Image></div>
                                 <Link href="/" className="flex items-center gap-5 custom-shaodw">aerialink.jp</Link>
                             </li>
-                            <li className="flex items-center gap-5">
+                            <li className="flex items-center gap-2.5 md:gap-5">
                                 <div className="bg-white rounded-full px-2.5 py-2"><Image alt="icon" src="/icons/address.png" width={25} height={25}></Image></div>
-                                <Link href="https://maps.app.goo.gl/x4bdhdoMwSufLe31A" target="_blank" className="flex items-center  gap-5 custom-shaodw">6E-02, 6-9 Koyochonaka, Higashinadaku, Kobe <br /> Hyogo 658-0032 Japan</Link>
+                                <Link href="https://maps.app.goo.gl/x4bdhdoMwSufLe31A" target="_blank" className="flex items-center  gap-5 custom-shaodw !w-[70%] md:w-[90%]">6E-02, 6-9 Koyochonaka, Higashinadaku, Kobe  Hyogo 658-0032 Japan</Link>
                             </li>
                         </ul>
 
@@ -43,7 +43,7 @@ export default function ContactPage({ imageUrl, title, text }) {
                             </p>
                         </div>
 
-                        <div className="absolute end-2.5 bottom-[2px]"><Image className="w-[80px]" alt="icon" src="/icons/qr-code.png" width={100} height={100}></Image></div>
+                        <div className="absolute end-2.5 md:bottom-[2px] bottom-[20px]"><Image className="w-[80px]" alt="icon" src="/icons/qr-code.png" width={100} height={100}></Image></div>
                     </div>
                     <div className="border-0 border-amber-50 rounded-2xl backdrop-blur-lg">
                         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.635822438208!2d135.2681585!3d34.6891397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008d7bbb2ec4fd%3A0x3fc8f7fe18f2b3bf!2s6-ch%C5%8Dme-9%20K%C5%8Dy%C5%8Dch%C5%8Dnaka%2C%20Higashinada%20Ward%2C%20Kobe%2C%20Hyogo%20658-0032%2C%20Japan!5e0!3m2!1sen!2s!4v1754675364488!5m2!1sen!2s" width="100%" height="380"
@@ -56,7 +56,7 @@ export default function ContactPage({ imageUrl, title, text }) {
                 </div>
             </div>
             <div className="contactForm whyChooseUsCardContents">
-                <h3 className="pb-3 text-3xl font-bold text-center">Inquiry Form</h3>
+                <h3 className="pb-3 text-[20px] md:text-3xl font-bold text-center">Inquiry Form</h3>
                 <ContactForm />
             </div>
             <Whatsapp />
